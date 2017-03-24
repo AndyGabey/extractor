d53039fbd0828a4962e8abc594fd22b9fe661152
Mark Muetzelfeldt <markmuetz@gmail.com>
2017-03-24 21:02:09 +0000

    Beef up check_data. Now collects stats on columns in use in the CSV files:
    Each time a column is in use it is counted, and the stats for each col are output.
    Lets you see fairly easily which cols are necessary to expose to the user.
    Suspect there is a fair amount of overlap between this and the cols harvested from
    the historical code. Will have to handle all cases though of course:
    getting cols from one CSV that aren't in the next, etc. etc.

--- Extractor/check_data.py
+++ Extractor/check_data.py
@@ -1,61 +1,125 @@
 import os
 import re
+import datetime as dt
 from collections import Counter, defaultdict, OrderedDict
+import csv
 
 PATTERNS = {
     'metfidas': [
         # '/mnt/data/metfidas/Level1/2015/2015-AVG5-104.csv'
-        ('metfidas1', re.compile('/mnt/data/metfidas/Level1/(?P<year>\d{4})/(?P<year2>\d{4})-AVG5-(?P<doy>\d{3}).csv')),
+        ('metfidas1',
+            re.compile('/mnt/data/metfidas/Level1/(?P<year>\d{4})/(?P<year2>\d{4})-AVG5-(?P<doy>\d{3}).csv'),
+            '/mnt/data/metfidas/Level1/{year}/{year}-AVG5-{doy}.csv',
+            'year_year2_doy'),
         # '/mnt/data/metfidas/Level1/2015/2015-SMP1-104.csv'
-        ('metfidas2', re.compile('/mnt/data/metfidas/Level1/(?P<year>\d{4})/(?P<year2>\d{4})-SMP1-(?P<doy>\d{3}).csv')),
+        ('metfidas2',
+            re.compile('/mnt/data/metfidas/Level1/(?P<year>\d{4})/(?P<year2>\d{4})-SMP1-(?P<doy>\d{3}).csv'),
+            '/mnt/data/metfidas/Level1/{year}/{year2}-SMP1-{doy}.csv',
+            'year_year2_doy'),
         # '/mnt/data/metfidas/Level1/2015/2015-MMX5-104.csv'
-        ('metfidas3', re.compile('/mnt/data/metfidas/Level1/(?P<year>\d{4})/(?P<year2>\d{4})-MMX5-(?P<doy>\d{3}).csv')),
+        ('metfidas3',
+            re.compile('/mnt/data/metfidas/Level1/(?P<year>\d{4})/(?P<year2>\d{4})-MMX5-(?P<doy>\d{3}).csv'),
+            '/mnt/data/metfidas/Level1/{year}/{year2}-MMX5-{doy}.csv',
+            'year_year2_doy'),
         # '/mnt/data/metfidas/Level2/1hour/2015/20150415_1hour.csv'
-        ('metfidas4', re.compile('/mnt/data/metfidas/Level2/1hour/(?P<year>\d{4})/(?P<date>\d{8})_1hour.csv')),
+        ('metfidas4',
+            re.compile('/mnt/data/metfidas/Level2/1hour/(?P<year>\d{4})/(?P<date>\d{8})_1hour.csv'),
+            '/mnt/data/metfidas/Level2/1hour/{year}/{date}_1hour.csv',
+            'year_date'),
         # '/mnt/data/metfidas/Level2/1hour/2015/20150415_1hour_maxmin.csv'
-        ('metfidas5', re.compile('/mnt/data/metfidas/Level2/1hour/(?P<year>\d{4})/(?P<date>\d{8})_1hour_maxmin.csv')),
+        ('metfidas5',
+            re.compile('/mnt/data/metfidas/Level2/1hour/(?P<year>\d{4})/(?P<date>\d{8})_1hour_maxmin.csv'),
+            '/mnt/data/metfidas/Level2/1hour/{year}/{date}_1hour_maxmin.csv',
+            'year_date'),
         # '/mnt/data/metfidas/Level2/5min/2015/20150415_5min.csv',
-        ('metfidas6', re.compile('/mnt/data/metfidas/Level2/5min/(?P<year>\d{4})/(?P<date>\d{8})_5min.csv')),
+        ('metfidas6',
+            re.compile('/mnt/data/metfidas/Level2/5min/(?P<year>\d{4})/(?P<date>\d{8})_5min.csv'),
+            '/mnt/data/metfidas/Level2/5min/{year}/{date}_5min.csv',
+            'year_date'),
         # '/mnt/data/metfidas/Level2/5min/2015/20150415_5min_maxmin.csv'
-        ('metfidas7', re.compile('/mnt/data/metfidas/Level2/5min/(?P<year>\d{4})/(?P<date>\d{8})_5min_maxmin.csv')),
+        ('metfidas7',
+            re.compile('/mnt/data/metfidas/Level2/5min/(?P<year>\d{4})/(?P<date>\d{8})_5min_maxmin.csv'),
+            '/mnt/data/metfidas/Level2/5min/{year}/{date}_5min_maxmin.csv',
+            'year_date'),
         # '/mnt/data/metfidas/Level2/profile/2015/20150819_UTprofile.csv'
-        ('metfidas8', re.compile('/mnt/data/metfidas/Level2/profile/(?P<year>\d{4})/(?P<date>\d{8})_UTprofile.csv')),
+        ('metfidas8',
+            re.compile('/mnt/data/metfidas/Level2/profile/(?P<year>\d{4})/(?P<date>\d{8})_UTprofile.csv'),
+            '/mnt/data/metfidas/Level2/profile/{year}/{date}_UTprofile.csv',
+            'year_date'),
     ],
     'labserver_files': [
         # '/mnt/data/labserver_files/METFiDAS/data/processed/derived/2005/2005D220.csv'
-        ('ls1', re.compile('/mnt/data/labserver_files/METFiDAS/data/processed/derived/(?P<year>\d{4})/(?P<year2>\d{4})D(?P<doy>\d{3}).csv')),
+        ('ls1',
+            re.compile('/mnt/data/labserver_files/METFiDAS/data/processed/derived/(?P<year>\d{4})/(?P<year2>\d{4})D(?P<doy>\d{3}).csv'),
+            '/mnt/data/labserver_files/METFiDAS/data/processed/derived/{year}/{year2}D{doy}.csv',
+            'year_year2_doy'),
         # '/mnt/data/labserver_files/METFiDAS/data/processed/measured/2005/2005M220.csv'
-        ('ls2', re.compile('/mnt/data/labserver_files/METFiDAS/data/processed/measured/(?P<year>\d{4})/(?P<year2>\d{4})M(?P<doy>\d{3}).csv')),
+        ('ls2',
+            re.compile('/mnt/data/labserver_files/METFiDAS/data/processed/measured/(?P<year>\d{4})/(?P<year2>\d{4})M(?P<doy>\d{3}).csv'),
+            '/mnt/data/labserver_files/METFiDAS/data/processed/measured/{year}/{year2}M{doy}.csv',
+            'year_year2_doy'),
         # '/mnt/data/labserver_files/METFiDAS-3/Level1/2014/2014-AVG5-237.csv'
-        ('ls3', re.compile('/mnt/data/labserver_files/METFiDAS-3/Level1/(?P<year>\d{4})/(?P<year2>\d{4})-AVG5-(?P<doy>\d{3}).csv')),
+        ('ls3',
+            re.compile('/mnt/data/labserver_files/METFiDAS-3/Level1/(?P<year>\d{4})/(?P<year2>\d{4})-AVG5-(?P<doy>\d{3}).csv'),
+            '/mnt/data/labserver_files/METFiDAS-3/Level1/{year}/{year2}-AVG5-{doy}.csv',
+            'year_year2_doy'),
         # '/mnt/data/labserver_files/METFiDAS-3/Level1/2014/2014-MMX5-237.csv'
-        ('ls4', re.compile('/mnt/data/labserver_files/METFiDAS-3/Level1/(?P<year>\d{4})/(?P<year2>\d{4})-MMX5-(?P<doy>\d{3}).csv')),
+        ('ls4',
+            re.compile('/mnt/data/labserver_files/METFiDAS-3/Level1/(?P<year>\d{4})/(?P<year2>\d{4})-MMX5-(?P<doy>\d{3}).csv'),
+            '/mnt/data/labserver_files/METFiDAS-3/Level1/{year}/{year2}-MMX5-{doy}.csv',
+            'year_year2_doy'),
         # '/mnt/data/labserver_files/METFiDAS-3/Level1/2014/2014-SMP1-237.csv'
-        ('ls5', re.compile('/mnt/data/labserver_files/METFiDAS-3/Level1/(?P<year>\d{4})/(?P<year2>\d{4})-SMP1-(?P<doy>\d{3}).csv')),
+        ('ls5',
+            re.compile('/mnt/data/labserver_files/METFiDAS-3/Level1/(?P<year>\d{4})/(?P<year2>\d{4})-SMP1-(?P<doy>\d{3}).csv'),
+            '/mnt/data/labserver_files/METFiDAS-3/Level1/{year}/{year2}-SMP1-{doy}.csv',
+            'year_year2_doy'),
         # '/mnt/data/labserver_files/METFiDAS-3/Level2/1hour/2014/20140831_1hour.csv',
-        ('ls6', re.compile('/mnt/data/labserver_files/METFiDAS-3/Level2/1hour/(?P<year>\d{4})/(?P<date>\d{8})_1hour.csv')),
+        ('ls6',
+            re.compile('/mnt/data/labserver_files/METFiDAS-3/Level2/1hour/(?P<year>\d{4})/(?P<date>\d{8})_1hour.csv'),
+            '/mnt/data/labserver_files/METFiDAS-3/Level2/1hour/{year}/{date}_1hour.csv',
+            'year_date'),
         # '/mnt/data/labserver_files/METFiDAS-3/Level2/1hour/2014/20140831_1hour_maxmin.csv',
-        ('ls7', re.compile('/mnt/data/labserver_files/METFiDAS-3/Level2/1hour/(?P<year>\d{4})/(?P<date>\d{8})_1hour_maxmin.csv')),
+        ('ls7',
+            re.compile('/mnt/data/labserver_files/METFiDAS-3/Level2/1hour/(?P<year>\d{4})/(?P<date>\d{8})_1hour_maxmin.csv'),
+            '/mnt/data/labserver_files/METFiDAS-3/Level2/1hour/{year}/{date}_1hour_maxmin.csv',
+            'year_date'),
         # '/mnt/data/labserver_files/METFiDAS-3/Level2/5min/2014/20140831_5min.csv',
-        ('ls8', re.compile('/mnt/data/labserver_files/METFiDAS-3/Level2/5min/(?P<year>\d{4})/(?P<date>\d{8})_5min.csv')),
+        ('ls8',
+            re.compile('/mnt/data/labserver_files/METFiDAS-3/Level2/5min/(?P<year>\d{4})/(?P<date>\d{8})_5min.csv'),
+            '/mnt/data/labserver_files/METFiDAS-3/Level2/5min/{year}/{date}_5min.csv',
+            'year_date'),
         # '/mnt/data/labserver_files/METFiDAS-3/Level2/5min/2014/20140831_5min_maxmin.csv',
-        ('ls9', re.compile('/mnt/data/labserver_files/METFiDAS-3/Level2/5min/(?P<year>\d{4})/(?P<date>\d{8})_5min_maxmin.csv')),
+        ('ls9',
+            re.compile('/mnt/data/labserver_files/METFiDAS-3/Level2/5min/(?P<year>\d{4})/(?P<date>\d{8})_5min_maxmin.csv'),
+            '/mnt/data/labserver_files/METFiDAS-3/Level2/5min/{year}/{date}_5min_maxmin.csv',
+            'year_date'),
     ],
     'SonicLicor-Incoming': [
         # '/mnt/data/SonicLicor-Incoming/Level1/2015/2015-SMP-223.csv'
-        ('sli1', re.compile('/mnt/data/SonicLicor-Incoming/Level1/(?P<year>\d{4})/(?P<year2>\d{4})-SMP-(?P<doy>\d{3}).csv')),
+        ('sli1',
+            re.compile('/mnt/data/SonicLicor-Incoming/Level1/(?P<year>\d{4})/(?P<year2>\d{4})-SMP-(?P<doy>\d{3}).csv'),
+            '/mnt/data/SonicLicor-Incoming/Level1/{year}/{year2}-SMP-{doy}.csv',
+            'year_year2_doy'),
     ],
     'Ceilometer-Incoming': [
         # '/mnt/data/Ceilometer-Incoming/Level2/1min/2016/20160604_1min.csv'
-        ('ci1', re.compile('/mnt/data/Ceilometer-Incoming/Level2/1min/(?P<year>\d{4})/(?P<date>\d{8})_1min.csv')),
+        ('ci1', 
+            re.compile('/mnt/data/Ceilometer-Incoming/Level2/1min/(?P<year>\d{4})/(?P<date>\d{8})_1min.csv'),
+            '/mnt/data/Ceilometer-Incoming/Level2/1min/{year}/{date}_1min.csv',
+            'year_date'),
         # '/mnt/data/Ceilometer-Incoming/Level2/5min/2014/20140501_5min.csv'
-        ('ci2', re.compile('/mnt/data/Ceilometer-Incoming/Level2/5min/(?P<year>\d{4})/(?P<date>\d{8})_5min.csv')),
+        ('ci2', 
+            re.compile('/mnt/data/Ceilometer-Incoming/Level2/5min/(?P<year>\d{4})/(?P<date>\d{8})_5min.csv'),
+            '/mnt/data/Ceilometer-Incoming/Level2/5min/{year}/{date}_5min.csv',
+            'year_date'),
     ],
     'LUMA': [
         # '/mnt/data/LUMA/RUAO/ReadingFlux_2015259.csv'
-        ('luma1', re.compile('/mnt/data/LUMA/RUAO/ReadingFlux_(?P<date_doy>\d{7}).csv')),
+        ('luma1', 
+            re.compile('/mnt/data/LUMA/RUAO/ReadingFlux_(?P<date_doy>\d{7}).csv'),
+            '/mnt/data/LUMA/RUAO/ReadingFlux_{date_doy}.csv',
+            'date_doy'),
     ],
-
 }
 
 
@@ -86,11 +150,11 @@ def check_filenames():
 
 
 def check_path(instrument_patterns, path):
-    for name, pattern in instrument_patterns:
+    for name, pattern, fmt, parse_opt in instrument_patterns:
         match = re.match(pattern, path)
         if match:
-            return name, pattern, match
-    return name, None, None
+            return name, pattern, fmt, parse_opt, match
+    return name, None, None, None, None
 
 
 def check_path_format(instrument_paths):
@@ -104,15 +168,140 @@ def check_path_format(instrument_paths):
             continue
 
         for path in paths:
-            name, pattern, match = check_path(instrument_patterns, path)
+            name, pattern, fmt, parse_opt, match = check_path(instrument_patterns, path)
             if not match:
                 # print('  Unmatched: {}'.format(path))
                 unmatched[instrument].append(path)
-            pattern_matches['{}:{}'.format(instrument, name)].append(match)
+                continue
+            pattern_matches['{}:{}'.format(instrument, name)].append((path, fmt, parse_opt, match))
         print('  Unmatched count: {}'.format(len(unmatched[instrument])))
     return pattern_matches, unmatched
 
 
 def check_date_parse(pattern_matches):
-    for name, matches in pattern_matches.items():
+    dates = {}
+    for name, parse_opt_matches in pattern_matches.items():
+        dates[name] = {'earliest': dt.datetime(2999, 1, 1), # Intentionally later than reasonable.
+                         'latest': dt.datetime(1, 1, 1)} # ditto.
         print(name)
+        for path, fmt, parse_opt, match in parse_opt_matches:
+            if 'fmt' not in dates[name]:
+                dates[name]['fmt'] = fmt
+                dates[name]['parse_opt'] = parse_opt
+            else:
+                assert dates[name]['fmt'] == fmt
+                assert dates[name]['parse_opt'] == parse_opt
+
+            if parse_opt == 'year_year2_doy':
+                assert len(match.group('year')) == 4
+                assert len(match.group('year2')) == 4
+                assert len(match.group('doy')) == 3
+
+                if match.group('year') != match.group('year2'):
+                    print('Years dont match!')
+                    print(path)
+                    continue
+                year_start = dt.datetime(int(match.group('year')), 1, 1)
+                doy = int(match.group('doy'))
+                date = year_start + dt.timedelta(days=doy - 1)
+            elif parse_opt == 'year_date':
+                assert len(match.group('year')) == 4
+                assert len(match.group('date')) == 8
+
+                year = int(match.group('year'))
+                date = dt.datetime.strptime(match.group('date'), '%Y%m%d')
+                if date.year != year:
+                    print('Years dont match!')
+                    print(path)
+                    continue
+            elif parse_opt == 'date_doy':
+                assert len(match.group('date_doy')) == 7
+
+                date = dt.datetime.strptime(match.group('date_doy'), '%Y%j')
+            else:
+                raise Exception('Unknown parse_opt {}'.format(parse_opt))
+            dates[name]['earliest'] = min(date, dates[name]['earliest'])
+            dates[name]['latest'] = max(date, dates[name]['latest'])
+    return dates
+
+
+def check_missing(dates):
+    files = defaultdict(list)
+    missing = []
+    for k, v in dates.items():
+        print(k)
+        date = v['earliest']
+        end = v['latest']
+        fmt = v['fmt']
+        parse_opt = v['parse_opt']
+
+        while date <= end:
+            if parse_opt == 'year_year2_doy':
+                filename = fmt.format(year=date.year, year2=date.year, doy=date.strftime('%j'))
+            elif parse_opt == 'year_date':
+                filename = fmt.format(year=date.year, date=date.strftime('%Y%m%d'))
+            elif parse_opt == 'date_doy':
+                filename = fmt.format(date_doy=date.strftime('%Y%j'))
+            else:
+                raise Exception('Unknown parse_opt {}'.format(parse_opt))
+
+            #print(filename)
+            if not os.path.exists(filename):
+                print('  MISSING DATE: {}'.format(date))
+                print('  FILENAME: {}'.format(filename))
+                missing.append(filename)
+            else:
+                files[k].append(filename)
+
+            date += dt.timedelta(days=1)
+
+    return files, missing
+
+def parse_header(files):
+    counters = {}
+    for k, filenames in files.items():
+        print(k)
+        counter = Counter()
+        for csv_file in filenames:
+            with open(csv_file, 'r') as f:
+                reader = csv.reader(f)
+                header = reader.next()
+            counter[','.join(header)] += 1
+            #print((csv_file, len(counter)))
+        print(counter)
+        counters[k] = counter
+    return counters
+
+
+def calc_set(counters):
+    for k, counter in counters.items():
+        c2 = Counter()
+        c3 = Counter()
+        print(k)
+        cols = []
+        all_cols = set()
+        any_cols = set()
+
+        for ck in counter.keys():
+            s = set(ck.split(','))
+            key = ','.join(sorted(s))
+            c2[key] += 1 
+            if not any_cols:
+                any_cols = s.copy()
+            cols.append(s)
+            all_cols |= s
+            any_cols &= s
+            print('    {}: {}'.format(counter[ck], ck))
+            for el in s:
+                c3[el] += counter[ck]
+
+        counts = []
+        for el, val in c3.items():
+            counts.append((el, val))
+
+        for count in sorted(counts, key=lambda c: c[1], reverse=True):
+            print('           {}: {}'.format(*count))
+        print('  Total combined headers: {}'.format(len(all_cols)))
+        print('  In every header:        {}'.format(len(any_cols)))
+        print('  Unique headers: {}'.format(len(counter)))
+        print('  Unique sets: {}'.format(len(c2)))
