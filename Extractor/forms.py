701ee516ab248f678ed0e20fc7ff6f99a024a0e3
Mark Muetzelfeldt <markmuetz@gmail.com>
2017-05-07 15:41:57 +0100

    Fix #3: Add extra info to start_date, end_date (and time_res for good measure) about DATE_FMT.

--- Extractor/forms.py
+++ Extractor/forms.py
@@ -14,9 +14,9 @@ class LoginForm(FlaskForm):
 class DatasetForm(FlaskForm):
     name = StringField('name', validators=[DataRequired()])
     long_name = StringField('long_name', validators=[DataRequired()])
-    start_date = DateTimeField('start_date', format=DATE_FMT, validators=[DataRequired()])
-    end_date = DateTimeField('end_date', format=DATE_FMT, validators=[DataRequired()])
-    time_res = StringField('time_res', validators=[DataRequired()])
+    start_date = DateTimeField('start_date (format={})'.format(DATE_FMT), format=DATE_FMT, validators=[DataRequired()])
+    end_date = DateTimeField('end_date (format="{}")'.format(DATE_FMT), format=DATE_FMT, validators=[DataRequired()])
+    time_res = StringField('time_res (timesteps per day)', validators=[DataRequired()])
     file_fmt = StringField('file_fmt', validators=[DataRequired()])
     date_col_name = StringField('date_col_name', validators=[DataRequired()])
     time_col_name = StringField('time_col_name', validators=[])
